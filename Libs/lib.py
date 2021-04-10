1b5ccbde556ebc7b308a235ad4fe466b3fe7f558
vHovvyan <vahagnhovvyan@gmail.com>
2021-04-10 14:21:58 +0400

    add Libs

---
+++ Libs/lib.py
@@ -0,0 +1,14 @@
+class Lib:
+
+    def writeToFile(self, links):
+        try:
+            with open('test_file.txt', 'a') as f:
+                for link in links:
+                    href = link.get_attribute('href')
+                    f.write(str(href) + "\n")
+        except (IOError, ValueError, EOFError) as error:
+            print(error)
+        except:
+            raise Exception("Unexpected error to write in file")
+        finally:
+            f.close()
