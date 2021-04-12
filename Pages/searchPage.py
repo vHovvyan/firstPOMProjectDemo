3442148fdc26a6ff4c8e33981fe51ec9ecb976cb
vHovvyan <vahagnhovvyan@gmail.com>
2021-04-12 19:24:41 +0400

    fix writ in file

--- Pages/searchPage.py
+++ Pages/searchPage.py
@@ -1,22 +1,20 @@
 from Locators.locators import Locators
+from Libs.lib import Lib
 
 
-class SearchPage():
+class SearchPage:
     def __init__(self, driver):
         self.driver = driver
 
         self.searchTextBoxName = Locators.searchTextBoxName
         self.searchButtonName = Locators.searchButtonName
 
-    def enter_seachword(self, search_word):
+    def enter_searchword(self, search_word):
         self.driver.find_element_by_name(self.searchTextBoxName).clear()
         self.driver.find_element_by_name(self.searchTextBoxName).send_keys(search_word)
 
     def click_search(self):
         self.driver.find_element_by_name(self.searchButtonName).click()
         links = self.driver.find_elements_by_xpath('.//div[@class="yuRUbf"]/a')
-        print(links)
-        with open('test_file.txt', 'a') as f:
-            for link in links:
-                href = link.get_attribute('href')
-                f.write(str(href) + "\n")
+        lib = Lib()
+        lib.writeToFile(links)
