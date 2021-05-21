af5c57fefe401bd325a509d9c981aa8e70afa429
vHovvyan <vahagnhovvyan@gmail.com>
2021-05-21 15:00:14 +0400

    update locators

---
+++ Libs/writeToFileLib.py
@@ -0,0 +1,15 @@
+class Lib:
+
+    def writeToFile(self, links):
+        try:
+            with open('test_file.txt', 'a') as f:
+                for link in links:
+                    href = link.get_attribute('href')
+                    f.write(str(href) + "\n")
+        except (IOError, ValueError, EOFError) as error:
+            print(error)
+            raise Exception("Unexpected error to write in file")
+        finally:
+            f.close()
+
+
--- Locators/locators.py
+++ Locators/locators.py
@@ -1,5 +1,8 @@
-class Locators():
-    searchTextBoxName = "q"
-    searchButtonName = "btnK"
+from selenium.webdriver.common.by import By
 
+class SearchPageLocators:
 
+    searchTextBoxName = (By.NAME, "q")
+    searchButtonName = (By.NAME, "btnK")
+    xpathLocators = (By.XPATH, './/div[@class="yuRUbf"]/a')
+    serachLenguge = (By.LINK_TEXT, "русский")
