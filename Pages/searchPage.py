c262a44676f11800273abd1f9a391c1a563a0523
vHovvyan <vahagnhovvyan@gmail.com>
2021-05-21 15:01:11 +0400

    update searchPage

--- Pages/searchPage.py
+++ Pages/searchPage.py
@@ -1,20 +1,23 @@
-from Locators.locators import Locators
-from Libs.lib import Lib
+from Locators.locators import SearchPageLocators
+from Libs.writeToFileLib import Lib
+from selenium.webdriver.common.by import By
 
 
 class SearchPage:
     def __init__(self, driver):
+
         self.driver = driver
 
-        self.searchTextBoxName = Locators.searchTextBoxName
-        self.searchButtonName = Locators.searchButtonName
+        #self.searchTextBoxName = Locators.searchTextBoxName
+        #self.searchButtonName = Locators.searchButtonName
 
-    def enter_searchword(self, search_word):
-        self.driver.find_element_by_name(self.searchTextBoxName).clear()
-        self.driver.find_element_by_name(self.searchTextBoxName).send_keys(search_word)
+    def enter_searchWord(self, search_word):
+        self.driver.find_element(*SearchPageLocators.searchTextBoxName).clear()
+        self.driver.find_element(*SearchPageLocators.searchTextBoxName).send_keys(search_word)
 
     def click_search(self):
-        self.driver.find_element_by_name(self.searchButtonName).click()
-        links = self.driver.find_elements_by_xpath('.//div[@class="yuRUbf"]/a')
+        self.driver.find_element(*SearchPageLocators.searchButtonName).click()
+        links = self.driver.find_elements(*SearchPageLocators.xpathLocators)
+        
         lib = Lib()
         lib.writeToFile(links)
