25c3e34bf4fb8247e83fa38b8bbda1ac4835d531
vHovvyan <81201506+vHovvyan@users.noreply.github.com>
2021-05-21 16:13:27 +0400

    Update locators.py
    xpath add

--- Locators/locators.py
+++ Locators/locators.py
@@ -3,6 +3,6 @@ from selenium.webdriver.common.by import By
 class SearchPageLocators:
 
     searchTextBoxName = (By.NAME, "q")
-    searchButtonName = (By.NAME, "btnK")
+    searchButtonName = (By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]')
     xpathLocators = (By.XPATH, './/div[@class="yuRUbf"]/a')
     serachLenguge = (By.LINK_TEXT, "русский")
